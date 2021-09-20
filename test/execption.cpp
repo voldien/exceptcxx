473528961dd8aa17e7123ab12122a9f8f78bd9e9
voldien <voldiekami@gmail.com>
2021-09-20 11:10:31 +0200

    Minor

--- test/execption.cpp
+++ test/execption.cpp
@@ -3,8 +3,10 @@
 
 int main(int argc, const char **argv) {
 	try {
-		throw cxxexcept::RuntimeException("");
+		throw cxxexcept::RuntimeException("{}", "hello");
 	} catch (cxxexcept::CaptureException &ex) {
 		std::cout << ex.what();
+
+		std::cout << ex.getName();
 	}
 }
